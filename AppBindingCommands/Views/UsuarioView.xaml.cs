a333e31dfd5428ef1bf74cd8df5677c713b70070
Alitor004 <irmaosgomes1302@gmail.com>
2023-08-18 22:16:00 -0300

    Criação do projeto AppBindingCommands

---
+++ AppBindingCommands/Views/UsuarioView.xaml.cs
@@ -0,0 +1,15 @@
+using AppBindingCommands.ViewModels;
+
+namespace AppBindingCommands.Views;
+
+public partial class UsuarioView : ContentPage
+{
+    private UsuarioViewModel viewModel;
+    public UsuarioView()
+	{
+		InitializeComponent();
+
+        viewModel = new UsuarioViewModel();
+        BindingContext = viewModel;
+    }
+}
